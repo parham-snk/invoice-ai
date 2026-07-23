8b5232d4aeea1f8ddde6858f3856d2be1c4b33f8
parham-snk <psanjanaki@gmail.com>
2026-07-24 01:27:08 +0330

    test2

--- src/router/routes/home/home.jsx
+++ src/router/routes/home/home.jsx
@@ -1,61 +1,38 @@
 import { useEffect, useState } from "react"
 import { FaPlay, FaPause } from "react-icons/fa";
-
+import speachRecognition,{useSpeechRecognition} from "react-speech-recognition"
 
 
 export default function Home() {
 
-    const [isPlay, setIsPlay] = useState(false);
-    const [data, setData] = useState();
+    const [isPlay, setIsPlay] = useState(false)
 
-
-    const SpeechRecognition =
-        window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new SpeechRecognition()
-    recognition.lang = "fa-IR"; // فارسی
-    recognition.continuous = true;
-    recognition.interimResults = true;
-
-
-    recognition.onresult = (eve) => {
-        let text = "";
-
-        for (let i = eve.resultIndex; i < eve.results.length; i++) {
-            text += eve.results[i][0].transcript;
+    const {browserSupportsSpeechRecognition,listening,transcript}=useSpeechRecognition()
+    useEffect(()=>{
+        if(isPlay){
+            speachRecognition.startListening({language:"fa-IR"})
+        }else{
+            speachRecognition.stopListening()
         }
-
-        setData(text)
-    }
-    useEffect(() => {
-
-    }, [])
-    useEffect(() => {
-        if (isPlay) {
-
-        }
-    }, [isPlay])
+    },[isPlay])
 
     return (
         <div className="bg-zinc-800 w-screen h-screen flex justify-center align-middle items-center flex-col">
             <div className="text-white ">
                 {
                     isPlay &&
-                    <FaPause size={100} className="animate-pulse cursor-pointer opacity-0" onClick={() => {
-
-                        setIsPlay(!isPlay)
-                        recognition.start()
-                    }} />
+                    <FaPause size={100} className="animate-pulse cursor-pointer opacity-0" onClick={()=>setIsPlay(!isPlay)}/>
                 }
                 {
-                    !isPlay &&
-                    <FaPlay size={100} className="animate-pulse cursor-pointer opacity-0" onClick={() => {
-                        setIsPlay(!isPlay)
-                    }} />
+                    !isPlay && 
+                    <FaPlay size={100} className="animate-pulse cursor-pointer opacity-0" onClick={()=>setIsPlay(!isPlay)}/>
                 }
             </div>
 
-            <div className="text-white ">
-                {data}
+            <div className="">
+                {
+                    trans
+                }
             </div>
         </div>
     )
